da64e0ad85768d1f1d92f9449c91b76d87f0baec
suman98 <41943773+suman98@users.noreply.github.com>
2026-02-22 16:04:28 +0545

    added laravel auth

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,33 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OllamaController;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-Route::prefix('ollama')->group(function () {
-    Route::post('/generate', [OllamaController::class, 'generate']);
-    Route::post('/chat', [OllamaController::class, 'chat']);
-    Route::get('/models', [OllamaController::class, 'models']);
-    Route::post('/generate-stream', [OllamaController::class, 'generateStream']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
+
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
+
